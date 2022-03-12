a3d71d4bb2c414c46df514c178d0eefeb709068c
RITESH8503 <rit8503@gmail.com>
2022-03-12 13:36:58 +0530

    M3

--- Implementation/SRC/Bike.java
+++ Implementation/SRC/Bike.java
@@ -7,24 +7,24 @@ package Implementation.SRC;
 public class Bike extends Vehicle {
 	private int engineSize;
 
-	//default constructor
+	
 	public Bike() {
 		super();
 	}
 
-	//constructor
+	
 	public Bike(String vehicleId, String vehicleBrand, int engineSize) {
 		super(vehicleId, vehicleBrand);
 		this.engineSize = engineSize;
 		//this.type = "Bike";
 	}
 
-	//getter for engine size
+
 	public int getEngineSize() {
 		return engineSize;
 	}
 
-	//setter for engine size
+	
 	public void setEngineSize(int engineSize) {
 		this.engineSize = engineSize;
 	}
